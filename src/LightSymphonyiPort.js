142c87e944d006b26edad609998deb7f9b3ebbb1
Ted Sales <ted@macrohard.co.uk>
2020-01-04 14:28:29 +0000

    Initial commit of code

---
+++ src/LightSymphonyiPort.js
@@ -0,0 +1,99 @@
+var dgram = require('dgram');
+
+var COMMAND_PORT = 10001;
+var DISCOVER_PORT = 30718;
+var DISCOVER_TIMEOUT = 1000;
+var MAX_NO_AREAS = 29;
+var MAX_INTENSITY = 10;
+
+module.exports = {
+  LightSymphonyiPort: LightSymphonyiPort
+};
+
+function LightSymphonyiPort(log, ipAddress) {
+  this.log = log;
+  this._ipAddress = ipAddress ? ipAddress : null; // iPort IP address set by _discoveriPort()
+}
+
+LightSymphonyiPort.prototype._discoveriPort = function (callback) {
+  var discovered = false;
+  var discoverMessage = Buffer.from([0x0, 0x1, 0x0, 0xf5]);
+  var validResponseMessage = Buffer.from('YES');
+  var broadcastAddress = '255.255.255.255';
+  var socket = dgram.createSocket('udp4');
+
+  socket.on('listening', function () {
+    socket.setBroadcast(true);
+  });
+
+  // Set up message event to handle discovery of iPort IP address
+  socket.on('message', function (response, remoteHost) {
+    this.log('_discoveriPort message event, response = ' + response.toString());
+    // Check for correct response and return address in callback
+    if (response.equals(validResponseMessage)) {
+      discovered = true;
+      socket.close();
+      this._ipAddress = remoteHost.address; // Set discovered iPort address
+      callback(null);
+    }
+  }.bind(this));
+
+  // Set up error function/event
+  var errorCallback = function (error) {
+    this.log('_discoveriPort errorCallback, discovered = ' + discovered.toString() + ', error = ' + error.toString());
+    if (!discovered) {
+      socket.close();
+      callback(error);
+    }
+  }.bind(this);
+  socket.on('error', errorCallback);
+
+  // Send discovery broadcast
+  socket.bind(DISCOVER_PORT);
+  socket.send(discoverMessage, 0, discoverMessage.length, DISCOVER_PORT, broadcastAddress);
+
+  // Call error function if no response after timeout
+  setTimeout(function () { errorCallback(new Error('iPort discover timed out')); }, DISCOVER_TIMEOUT);
+};
+
+LightSymphonyiPort.prototype._sendCommand = function (command, callback) {
+  var sendCommand = function () {
+    this.log('Sending: ' + command + ' to ' + this._ipAddress);
+    var socket = dgram.createSocket('udp4');
+    socket.send(command + '\r\n', COMMAND_PORT, this._ipAddress, function (error) {
+      socket.close();
+      callback(error);
+    });
+  }.bind(this);
+
+  if (this._ipAddress) {
+    // iPort IP address available, send command
+    sendCommand();
+  } else {
+    // iPort IP address unknown, attempt to discover it
+    this._discoveriPort(function (error) {
+      if (error) callback(error);
+      else sendCommand();
+    }.bind(this));
+  }
+};
+
+LightSymphonyiPort.prototype.setAll = function (on, callback) {
+  this._sendCommand('all_' + (on ? 'on' : 'off'), callback);
+};
+
+LightSymphonyiPort.prototype.setArea = function (area, on, callback) {
+  if (area < 1 || area > MAX_NO_AREAS)
+    callback(new Error('Area number ' + area.toString() + ' is invalid, must be between 1 and ' + MAX_NO_AREAS.toString()));
+  else
+    this._sendCommand('area_' + (on ? 'on' : 'off') + ' ' + area.toString(), callback);
+};
+
+LightSymphonyiPort.prototype.setIntensity = function (area, intensity, callback) {
+  if (area < 1 || area > MAX_NO_AREAS)
+    callback(new Error('Area number ' + area.toString() + ' is invalid, must be between 1 and ' + MAX_NO_AREAS.toString()));
+  else if (intensity < 1 || intensity > MAX_INTENSITY)
+    callback(new Error('Intensity value ' + intensity.toString() + ' is invalid, must be between 1 and ' + MAX_INTENSITY.toString()));
+  else
+    this._sendCommand('inten_' + area.toString() + '_' + intensity.toString(), callback);
+};
---
+++ src/index.js
@@ -0,0 +1,117 @@
+var LightSymphonyiPort = require('./LightSymphonyiPort').LightSymphonyiPort;
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory('homebridge-light-symphony', 'Light Symphony', LightSymphonyAccessory);
+};
+
+function LightSymphonyAccessory(log, config) {
+  this.log = log;
+
+  this._serialNumber = config.serialNumber;
+
+  this._lightSymphonyiPort = new LightSymphonyiPort(log, config.ipAddress);
+
+  this._areas = {};
+  this._areaServices = [];
+  for (var areaConfig of config.areas) {
+    if (areaConfig.number === 0 && areaConfig.dimmable)
+      throw Error('Dimmable all lights area (number 0) not currently supported');
+
+    // Create area with default states
+    var area = {
+      number: areaConfig.number,
+      name: areaConfig.name,
+      type: areaConfig.type,
+      dimmable: areaConfig.dimmable,
+      on: false,
+      brightness: 0
+    };
+
+    // Store in areas object
+    var areaKey = 'area_' + area.number.toString();
+    this._areas[areaKey] = area;
+
+    // Create HomeKit service for area
+    var areaService;
+    if (area.type === 'Switch')
+      areaService = new Service.Switch(area.name, areaKey);
+    else
+      areaService = new Service.Lightbulb(area.name, areaKey);
+    areaService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this._getAreaOn.bind(this, area.number))
+      .on('set', this._setAreaOn.bind(this, area.number));
+    if (area.dimmable) {
+      areaService
+        .getCharacteristic(Characteristic.Brightness)
+        .on('get', this._getAreaBrightness.bind(this, area.number))
+        .on('set', this._setAreaBrightness.bind(this, area.number));
+    }
+    this._areaServices.push(areaService);
+    area.service = areaService;
+  }
+}
+
+LightSymphonyAccessory.prototype._getAreaOn = function (areaNumber, callback) {
+  callback(null, this._areas['area_' + areaNumber.toString()].on);
+};
+
+LightSymphonyAccessory.prototype._setAreaOn = function (areaNumber, on, callback) {
+  if (areaNumber === 0) {
+    this._lightSymphonyiPort.setAll(on, function (error) {
+      if (!error) {
+        for (var areaKey in this._areas) {
+          if (this._areas.hasOwnProperty(areaKey)) {
+            this._areas[areaKey].on = on;
+            this._areas[areaKey].service.updateCharacteristic(Characteristic.On, on);
+          }
+        }
+      }
+      callback(error);
+    }.bind(this));
+  } else {
+    this._lightSymphonyiPort.setArea(areaNumber, on, function (error) {
+      if (!error) {
+        this._areas['area_' + areaNumber.toString()].on = on;
+        if (this._areas.hasOwnProperty('area_0')) {
+          var allOn = true;
+          for (var areaKey in this._areas) {
+            if (areaKey !== 'area_0' && this._areas.hasOwnProperty(areaKey))
+              allOn = allOn && this._areas[areaKey].on;
+          }
+          this.log('Setting area 0 to ' + (allOn ? 'on' : 'off'));
+          this._areas['area_0'].on = allOn;
+          this._areas['area_0'].service.updateCharacteristic(Characteristic.On, allOn);
+        }
+      }
+      callback(error);
+    }.bind(this));
+  }
+};
+
+LightSymphonyAccessory.prototype._getAreaBrightness = function (areaNumber, callback) {
+  callback(null, this._areas['area_' + areaNumber.toString()].brightness);
+};
+
+LightSymphonyAccessory.prototype._setAreaBrightness = function (areaNumber, brightness, callback) {
+  var intensity = Math.trunc(brightness / 10);
+  this._lightSymphonyiPort.setIntensity(areaNumber, intensity, function (error) {
+    if (!error)
+      this._areas['area_' + areaNumber.toString()].brightness = brightness;
+    callback(error);
+  }.bind(this));
+};
+
+LightSymphonyAccessory.prototype.getServices = function () {
+  var informationService = new Service.AccessoryInformation();
+  informationService
+    .setCharacteristic(Characteristic.Manufacturer, 'Light Symphony')
+    .setCharacteristic(Characteristic.Model, 'Controller')
+    .setCharacteristic(Characteristic.SerialNumber, this._serialNumber);
+  
+  return [informationService].concat(this._areaServices);
+};
